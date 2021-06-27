81b873be1f0632966c24aa5040c50af4d7e8a1e2
Danielgarram <daniegarcia10@gmail.com>
2021-06-27 00:58:42 -0500

    FEAT - App login:
    	Add login style and view
    
    Signed-off-by: Danielgarram <daniegarcia10@gmail.com>

--- app/mobey/src/app/views/pages/login/login.page.ts
+++ app/mobey/src/app/views/pages/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  @ViewChild('emailInput') emailInput: IonInput;
+  @ViewChild('passwordInput') passwordInput: IonInput;
 
   constructor() { }
+
+  loginUser = {
+    username: '',
+    password: ''
+  };
 
   ngOnInit() {
   }
 
+  doLogin(formLogin: NgForm) {
+    console.log('doLogin',formLogin.hasError('required', 'password'))
+    if (formLogin.invalid) {
+      if (formLogin.hasError('required', 'password')) {
+        this.passwordInput.setFocus();
+      }
+
+      if (formLogin.hasError('required', 'username')) {
+        this.emailInput.setFocus();
+      }
+      return;
+    }
+  }
 }
